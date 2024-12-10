3a4fcf30c7509b218bc7eed4292af84c66d9e45c
Felipe Triana <felipetrianago@gmail.com>
2024-12-10 14:28:28 -0500

    Se añade prueba unitaria de dudosa fiabilidad (Usa WebClient para consumir los endpoint)

--- src/test/java/com/course/WebFluxRestApi/WebFluxRestApiApplicationTests.java
+++ src/test/java/com/course/WebFluxRestApi/WebFluxRestApiApplicationTests.java
@@ -1,13 +1,38 @@
 package com.course.WebFluxRestApi;
 
+import com.course.WebFluxRestApi.models.documents.Producto;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import java.util.List;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class WebFluxRestApiApplicationTests {
+
+	@Autowired
+	private WebTestClient client;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void listarTest(){
+		client.get().uri("/api/v2/productos")
+				.exchange()
+				.expectStatus().isOk()
+				.expectHeader().contentType("application/json")
+				.expectBodyList(Producto.class)
+				.consumeWith(response -> {
+					List<Producto> productos = response.getResponseBody();
+					productos.forEach(p -> {
+						System.out.println(p.getNombre());
+					});
+					Assertions.assertTrue(productos.size() > 0);
+				});
 	}
 
 }
